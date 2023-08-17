a5115fb8b8c770251bb2e720cbca62965d6c8912
Daniel Sweet <daniel@Daniels-MacBook-Pro.local>
2023-08-17 12:46:19 +0200

    §	new file:   sounds/grand.mp3

---
+++ script.js
@@ -0,0 +1,35 @@
+const soundboard = document.querySelector(".soundboard");
+
+const soundFiles = [
+    "arrnarr.mp3",
+    "bestboy.mp3",
+    "butt.mp3",
+    "deezenuts.mp3",
+    "dj.mp3",
+    "grand.mp3",
+    "idontlikeit.mp3",
+    "newthing.mp3",
+    "niki.mp3",
+    "okkrr.mp3",
+    "on.mp3",
+    "piss.mp3",
+    "puuj.mp3",
+    "rutro.mp3",
+    "sogud.mp3",
+    "sorry.mp3",
+    "sss.mp3",
+    "wrecked.mp3"
+];
+
+soundFiles.forEach(soundFile => {
+  const tile = document.createElement("div");
+  tile.className = "tile";
+  tile.textContent = soundFile.replace(".mp3", ""); // Display name without extension
+  
+  tile.addEventListener("click", () => {
+    const audio = new Audio(`sounds/${soundFile}`);
+    audio.play();
+  });
+  
+  soundboard.appendChild(tile);
+});
